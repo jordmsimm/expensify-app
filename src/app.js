caea2f9382f476b0a911b4ca854c65ad3d96cdd3
Jordan <jordan@execad.com>
2018-06-07 00:54:42 -0400

    break reducers to separate files

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-import AppRouter from "./routers/AppRouter"
+import AppRouter from "./routers/AppRouter";
+import configureStore from './store/configureStore'
+import {addExpense} from './actions/expenses'
+import {setTextFilter }from './actions/filters'
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+const store = configureStore();
+
+store.dispatch(addExpense({description:'Heat Bill',amount:100, createdAt:1000}))
+store.dispatch(addExpense({description:'Water Bill',amount:150, createdAt:1000}))
+
+store.dispatch(setTextFilter('water'));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+console.log(visibleExpenses);
+
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'))
 
--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -135,7 +135,7 @@ const getVisibleExpenses = (expenses, {text,sortBy,startDate,endDate}) => {
         if(sortBy === 'date'){
             return a.createdAt < b.createdAt ? 1 : -1;
         }
-        if(sortBy === 'amount'){
+        else if(sortBy === 'amount'){
             return a.amount < b.amount ? 1 : -1;
         }
     })
